e4fe3f6a3e522ceef33348614fe5bc04f6013dca
Ravi Amarala <c083187@Ravis-MacBook-Pro.local>
2024-04-25 16:48:29 -0600

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+S
+#src/main/java/org/example/Main.java,0/b/0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+T
+$src/test/java/tests/SampleTests.java,4/a/4ad7c748b0e25529880221f1a2cc032e8aec36da
---
+++ src/test/java/tests/SampleTests.java
@@ -0,0 +1,18 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class SampleTests {
+
+    @Test
+    public void tesFirst(){
+        System.out.println("tesFirst");
+
+    }
+
+    @Test
+    public void testSecond(){
+        System.out.println("testSecond");
+
+    }
+}
